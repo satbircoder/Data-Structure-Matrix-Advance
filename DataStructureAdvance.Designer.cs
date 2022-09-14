6cf601ce26e4b053271a17dbd848aefec5581c38
SATBIR SINGH <satbirsingh.singh4@gmail.com>
2022-09-14 13:37:40 +0800

    Add project files.

---
+++ DataStructureAdvance.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Data_Structure_Matrix_Advance
+{
+    partial class FormDataStructureMatrixAdvance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormDataStructureMatrixAdvance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(528, 450);
+            this.Name = "FormDataStructureMatrixAdvance";
+            this.Text = "Data Structure Matrix Advance";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
